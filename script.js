21ac13b300bdecacad13313cf4046457496a5244
Steven Spronk <stevenspronk@Stevens-iMac.local>
2020-06-18 14:13:46 +0200

    Added Lodash

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+
+var lodash = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('awnser:',lodash.without(array,3));
+
 var css = document.querySelector("h3");
 var input1 = document.getElementsByName("color1")[0];
 var input2 = document.getElementsByName("color2")[0];
